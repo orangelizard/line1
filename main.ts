a326184af4f2e0fcfb365fef898a27819237c421
orangelizard <email@jasoncrosier.com>
2025-11-10 09:51:43 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,4 +7,34 @@ input.onButtonPressed(Button.B, function () {
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showString("" + (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.M)))
 })
+basic.showLeds(`
+    # . . # .
+    # . # . #
+    # . # . #
+    # . # . #
+    . # . . #
+    `)
+Acebott.stopcar()
 Acebott.Trace_Sensor_init(AnalogReadPin.P0, AnalogReadPin.P1, AnalogReadPin.P2)
+basic.forever(function () {
+    if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) > 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) > 500) {
+        Acebott.moveTime(Acebott.Direction.backward, 40)
+        basic.pause(100)
+        Acebott.moveTime(Acebott.Direction.left, 40)
+        basic.pause(100)
+    } else {
+        if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) > 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) < 500) {
+            Acebott.moveTime(Acebott.Direction.backward, 10)
+            basic.pause(100)
+            Acebott.moveTime(Acebott.Direction.left, 10)
+            basic.pause(100)
+        } else if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) < 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) > 500) {
+            Acebott.moveTime(Acebott.Direction.backward, 10)
+            basic.pause(100)
+            Acebott.moveTime(Acebott.Direction.right, 10)
+            basic.pause(100)
+        } else {
+            Acebott.moveTime(Acebott.Direction.forward, 10)
+        }
+    }
+})
