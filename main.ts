2c075bfa185e31ebb26cf3ca9e69087806650ae2
orangelizard <email@jasoncrosier.com>
2025-11-10 11:36:59 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("" + (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R)))
+    music.ringTone(988)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("" + (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L)))
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("" + (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.M)))
+	
 })
 basic.showLeds(`
     # . . # .
@@ -14,27 +11,21 @@ basic.showLeds(`
     # . # . #
     . # . . #
     `)
+Acebott.Trace_Sensor_init(AnalogReadPin.P0, AnalogReadPin.P2, AnalogReadPin.P1)
 Acebott.stopcar()
-Acebott.Trace_Sensor_init(AnalogReadPin.P0, AnalogReadPin.P1, AnalogReadPin.P2)
 basic.forever(function () {
-    if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) > 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) > 500) {
-        Acebott.moveTime(Acebott.Direction.backward, 40)
-        basic.pause(100)
-        Acebott.moveTime(Acebott.Direction.left, 40)
-        basic.pause(100)
-    } else {
-        if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) > 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) < 500) {
-            Acebott.moveTime(Acebott.Direction.backward, 10)
-            basic.pause(100)
-            Acebott.moveTime(Acebott.Direction.left, 10)
-            basic.pause(100)
-        } else if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) < 500 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) > 500) {
-            Acebott.moveTime(Acebott.Direction.backward, 10)
-            basic.pause(100)
-            Acebott.moveTime(Acebott.Direction.right, 10)
-            basic.pause(100)
-        } else {
-            Acebott.moveTime(Acebott.Direction.forward, 10)
-        }
+    if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) > 400) {
+        music.ringTone(988)
+        Acebott.colorLight(Acebott.RGBLights.RGB_R, 0xff0000)
+        Acebott.colorLight(Acebott.RGBLights.RGB_L, 0x000000)
+    }
+    if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) > 400) {
+        music.ringTone(131)
+        Acebott.colorLight(Acebott.RGBLights.RGB_L, 0x00ff00)
+        Acebott.colorLight(Acebott.RGBLights.RGB_R, 0x000000)
+    }
+    if (Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.R) < 400 && Acebott.Trace_Sensor_getValue(Trace_Sensor_Index.L) < 400) {
+        music.stopAllSounds()
+        Acebott.colorLight(Acebott.RGBLights.ALL, 0x000000)
     }
 })
